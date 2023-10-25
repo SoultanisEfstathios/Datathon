6f26cb9ba18f6c91c29550f9d132037e5860d8e5
SoultanisEfstathios <131766949+SoultanisEfstathios@users.noreply.github.com>
2023-10-25 15:00:31 +0300

    test

---
+++ sitejss.js
@@ -0,0 +1,50 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 4000); 
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+  let displayImage = document.getElementById('image1');
+  if(displayImage.src.match('./favicon.png')){
+    displayImage.src = './thelogo1.jpg'
+  }else {
+    displayImage.src = './favicon.png'
+  }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
